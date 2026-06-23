5099064f368c9373240e897609520d542a792ba4
Claire <claire-carlier@hotmail.fr>
2026-06-23 15:24:57 +0200

    Add 3-print_alphabets.c

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: prints the alphabet in lowercase and then in uppercase,
+ * followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char n;
+	n = 'a';
+	while (n <= 'z')
+	{
+		putchar(n);
+		n++;
+	}
+	n = 'A';
+	while (n <= 'Z')
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
