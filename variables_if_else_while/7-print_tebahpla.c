6d36dd0f797d9eef049f9db305fc7ab59729158b
Claire <claire-carlier@hotmail.fr>
2026-06-24 10:03:48 +0200

    Fix 7-print_tebahpla.c

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
- /**
+/**
  * main - entry point
- * Description: Write a program that prints the lowercase
- * alphabet in reverse, followed by a new line.
+ * Description: prints the lowercase alphabet in reverse,
+ * followed by a new line
  *
- * Return: Always 0 (Success) 
- */ 
- int main(void)
- 
- { 
- char n; 
- n = 'z'; 
- while (n >= 'a') 
- { 
- putchar(n); 
- n--;
- }
- putchar('\n');  
- return (0); 
- }
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char n;
+
+	n = 'z';
+	while (n >= 'a')
+	{
+		putchar(n);
+		n--;
+	}
+	putchar('\n');
+	return (0);
+}
