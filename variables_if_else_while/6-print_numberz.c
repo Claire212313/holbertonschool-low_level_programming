062bf9eb01f7c2686cade8dc7d0d77192e4d1da5
Claire <claire-carlier@hotmail.fr>
2026-06-24 09:38:52 +0200

    Fix 6-print_numberz.c

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
  * main - entry point
- * Description: prints all single digit numbers of base 10 starting from 0, followed by a new line
+ * Description: prints all single digit numbers of base 10
+ * starting from 0, followed by a new line
  *
  * Return: Always 0 (Success)
  */
