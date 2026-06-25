1d179df22cf1a19cf63027f25672f48540c05de4
Claire <claire-carlier@hotmail.fr>
2026-06-25 17:20:11 +0200

    Add 9-times_table.c

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table
+ *
+ * Return: void
+ */
+void times_table(void)
+{
+	int table, table_count, result;
+	for (table = 0; table <= 9; table++)
+	{
+		for (table_count = 0; table_count <= 9; table_count++)
+		{
+			result = table * table_count;
+			if (table_count == 0)
+			{
+				_putchar('0' + result);
+			}
+			else if (result <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + result);
+			}
+			else if (result >= 9)
+			{
+				_putchar(',');
+                                _putchar(' ');
+                                _putchar('0' + (result / 10));
+				_putchar('0' + (result % 10));
+			}
+		}
+	_putchar ('\n');
+	}
+}
