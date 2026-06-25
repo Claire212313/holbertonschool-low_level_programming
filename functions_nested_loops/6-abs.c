6f028295bb07fc2c648361bb4464b10020778ebc
Claire <claire-carlier@hotmail.fr>
2026-06-25 14:02:41 +0200

    Add 6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * int _abs -Write a function that computes the absolute value of an integer.
+ *
+ * @n: the number to check
+ *
+ * Return: 1 if lowercase, 0 otherwise
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	return (n);
+}
