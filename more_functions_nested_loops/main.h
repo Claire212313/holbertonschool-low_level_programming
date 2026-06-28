8bcaf7195a970a016869ff61f774410ee46d7288
Claire <claire-carlier@hotmail.fr>
2026-06-28 16:45:12 +0200

    Fix main.h

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -2,16 +2,11 @@
 #define MAIN_H
 
 int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int n);
-void jack_bauer(void);
-void times_table(void);
 int add(int a, int b);
-void print_to_98(int n);
 int print_last_digit(int n);
 int _isupper(int c);
 int _isdigit(int c);
@@ -22,6 +17,11 @@ void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
-void print_triangle(int size)
+void print_triangle(int size);
+void jack_bauer(void);
+void times_table(void);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+void print_to_98(int n);
 
 #endif
