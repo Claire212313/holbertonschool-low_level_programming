6f44e86cfc81923796a5e9d056cde3bf02623540
Claire <claire-carlier@hotmail.fr>
2026-07-01 14:59:10 +0200

    Fix 6-puts2.c

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -11,20 +11,11 @@ void puts2(char *str)
 	int i;
 	int len;
 
-	/* for used here because len is known before looping */
-	/* str += 2 with while is dangerous: can skip over \0 */
-	len = _strlen(str);
-	for (i = 0; i < len; i++)
-	{
-		_putchar(str[i]);
-		i++;
-	}
-	_putchar('\n');
-
-	/* EXAMPLE with while - DANGEROUS if string has odd length */
-	/* while (*str != '\0')                                    */
-	/* {                                                       */
-	/*     _putchar(*str);                                     */
-	/*     str += 2;  <- can jump over \0 !                   */
-	/* }                                                       */
+	while (*str != '\0')
+{
+    _putchar(*str);  /* affiche le caractère */
+    str++;           /* avance d'un */
+    if (*str != '\0')  /* vérifie qu'on n'est pas à la fin */
+        str++;       /* avance encore d'un */
 }
+_putchar('\n');
