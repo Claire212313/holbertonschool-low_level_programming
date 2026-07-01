ae7b0cf1c569c80b7e6f9552b3644fd640f8cd8c
Claire <claire-carlier@hotmail.fr>
2026-07-01 14:51:54 +0200

    Fix 6-puts2.c

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,16 +1,30 @@
-#include "main.h" /**
+#include "main.h"
 
-* puts2 - prints a string
-* @str: pointer to a string
-*
-* Return: void */
+/**
+ * puts2 - prints every other character of a string
+ * @str: pointer to a string
+ *
+ * Return: void
+ */
 void puts2(char *str)
 {
+	int i;
+	int len;
 
-	while (*str != '\0')
+	/* for used here because len is known before looping */
+	/* str += 2 with while is dangerous: can skip over \0 */
+	len = _strlen(str);
+	for (i = 0; i < len; i++)
 	{
-	_putchar(*str);
-	str+= 2;
+		_putchar(str[i]);
+		i++;
 	}
 	_putchar('\n');
+
+	/* EXAMPLE with while - DANGEROUS if string has odd length */
+	/* while (*str != '\0')                                    */
+	/* {                                                       */
+	/*     _putchar(*str);                                     */
+	/*     str += 2;  <- can jump over \0 !                   */
+	/* }                                                       */
 }
