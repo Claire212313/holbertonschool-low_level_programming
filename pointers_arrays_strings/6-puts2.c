941fcf1db9c1cb6fffea0fa2d565c18df75f2248
Claire <claire-carlier@hotmail.fr>
2026-07-01 14:26:55 +0200

    Add 6-puts2.c

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h" /**
+
+* puts2 - prints a string
+* @str: pointer to a string
+*
+* Return: void */
+void puts2(char *str)
+{
+
+	while (*str != '\0')
+	{
+	_putchar(*str);
+	str+= 2;
+	}
+	_putchar('\n');
+}
